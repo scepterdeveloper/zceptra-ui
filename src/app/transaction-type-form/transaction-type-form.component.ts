11cec37d72a7b5ea184902398abb56098349861a
Sandeep Joseph <sandeep.josef@gmail.com>
2019-01-19 00:23:14 +0100

    Selecting items in select for categories and accounts list

--- src/app/transaction-type-form/transaction-type-form.component.ts
+++ src/app/transaction-type-form/transaction-type-form.component.ts
@@ -20,21 +20,6 @@ const httpOptions = {
   })
 };
 
-
-export interface Pokemon {
-  value: string;
-  viewValue: string;
-}
-
-export interface PokemonGroup {
-  disabled?: boolean;
-  name: string;
-  pokemon: Pokemon[];
-}
-
-
-
-
 @Component({
   selector: 'app-transaction-type-form',
   templateUrl: './transaction-type-form.component.html',
@@ -42,48 +27,10 @@ export interface PokemonGroup {
 })
 export class TransactionTypeFormComponent implements OnInit {
 
-
-  pokemonControl = new FormControl();
-   pokemonGroups: PokemonGroup[] = [
-     {
-       name: 'Grass',
-       pokemon: [
-         {value: 'bulbasaur-0', viewValue: 'Bulbasaur'},
-         {value: 'oddish-1', viewValue: 'Oddish'},
-         {value: 'bellsprout-2', viewValue: 'Bellsprout'}
-       ]
-     },
-     {
-       name: 'Water',
-       pokemon: [
-         {value: 'squirtle-3', viewValue: 'Squirtle'},
-         {value: 'psyduck-4', viewValue: 'Psyduck'},
-         {value: 'horsea-5', viewValue: 'Horsea'}
-       ]
-     },
-     {
-       name: 'Fire',
-       disabled: true,
-       pokemon: [
-         {value: 'charmander-6', viewValue: 'Charmander'},
-         {value: 'vulpix-7', viewValue: 'Vulpix'},
-         {value: 'flareon-8', viewValue: 'Flareon'}
-       ]
-     },
-     {
-       name: 'Psychic',
-       pokemon: [
-         {value: 'mew-9', viewValue: 'Mew'},
-         {value: 'mewtwo-10', viewValue: 'Mewtwo'},
-       ]
-     }
-   ];
-
-   toppings = new FormControl();
-   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-
-
-
+  selectedAccounts: number[] = [2, 5, 8];
+  accountsControl = new FormControl(this.selectedAccounts);
+  selectedCategories: number[] = [1, 7];
+  categoriesControl = new FormControl(this.selectedCategories);
 
   transactionType: TransactionType;
   categories: Category[];
